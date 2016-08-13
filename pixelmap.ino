276e8fe63af95f6372c7f1fb8ebdfeac206ab27f
Nick Tzaperas <nick@nijotz.com>
2016-08-13 01:00:58 -0700

    Example ino file switches between animations via button press

--- pixelmap.ino
+++ pixelmap.ino
@@ -7,19 +7,20 @@
 using namespace pixelmap;
 
 RandomInput input = RandomInput();
-FireVisualization viz = FireVisualization(&input, 128);
 LEDStrip strip = LEDStrip(128);
 LEDs leds = LEDs(strip, 0, 128);
-PassThroughAnimation passthru = PassThroughAnimation(&viz, leds);
+
+Visualization* viz;
+Animation* anim;
+
+ButtonInput button = ButtonInput(5);
+
+int currentAnimation = 0;
+bool firstRun = true;
 
 void setup() {
   Log.Init(LOGLEVEL, 9600);
   Log.Info("setup()");
-
-  Looper* looper = Looper::instance();
-  looper->addInput(&input);
-  looper->addVisualization(&viz);
-  looper->addAnimation(&passthru);
 
   FastLED.addLeds<NEOPIXEL, 6>(strip.leds, 128);
   FastLED.show();
@@ -28,5 +29,39 @@ void setup() {
 }
 
 void loop() {
-  Looper::instance()->loop();
+  Looper* looper = Looper::instance();
+
+  if (button.getInput() > 0.5 || firstRun == true) {
+    Log.Info("Button press detected");
+    switch(currentAnimation) {
+      case 0:
+        Log.Info("Switching to Disco animation");
+        looper->clearAll();
+        delete viz;
+        delete anim;
+        viz = new NullVisualization(&input, 128);
+        anim = new DiscoAnimation(viz, leds);
+        looper->addInput(&input);
+        looper->addVisualization(viz);
+        looper->addAnimation(anim);
+        currentAnimation = 1;
+        break;
+      case 1:
+        Log.Info("Switching to Fire animation");
+        looper->clearAll();
+        delete viz;
+        delete anim;
+        viz = new FireVisualization(&input, 128);
+        anim = new PassThroughAnimation(viz, leds);
+        looper->addInput(&input);
+        looper->addVisualization(viz);
+        looper->addAnimation(anim);
+        currentAnimation = 0;
+        break;
+    }
+  }
+
+  firstRun = false;
+
+  looper->loop();
 }
