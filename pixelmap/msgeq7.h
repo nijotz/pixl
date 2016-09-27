f135a61e2ca94373f834e35226ed2a7c926174ae
Nick Tzaperas <nick@nijotz.com>
2016-09-27 14:18:16 -0700

    Support amp and freq on the MSGEQ7

--- pixelmap/msgeq7.h
+++ pixelmap/msgeq7.h
@@ -5,17 +5,28 @@ namespace pixelmap {
 
 class MSGEQ7Input : public Input {
   public:
-    MSGEQ7Input(int left_pin, int right_pin, int strobe_pin, int reset_pin, int max_amplitude, int min_amplitude);
+    MSGEQ7Input(int left_pin, int right_pin,
+                int strobe_pin, int reset_pin,
+                float min_amp, float max_amp,
+                float min_freq = 0, float max_freq = 0,
+                bool amplitude = true, bool mono = true);
     void update();
-    float getInput();
+    float getInput(int index = 0);
   private:
+    void updateAmplitude();
+    void updateFrequency();
     int strobe_pin_;
     int reset_pin_;
     int left_pin_;
     int right_pin_;
-    int max_amplitude_;
-    int min_amplitude_;
-    float value_;
+    float min_amp_;
+    float max_amp_;
+    float min_freq_;
+    float max_freq_;
+    int amplitude_;
+    int mono_;
+    float amp_;
+    float freq_;
 };
 
 } // end namespace pixelmap
