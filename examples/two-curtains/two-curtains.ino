81a91335c99a02c4278562d172049dbffd6db8a2
Nick Tzaperas <nick@nijotz.com>
2016-12-08 19:56:27 -0600

    Two curtains example

---
+++ examples/two-curtains/two-curtains.ino
@@ -0,0 +1,110 @@
+#include <pixelmap.h>
+#include <FastLED.h>
+#include <Logging.h>
+
+// Uncomment to turn off assertions
+#define NDEBUG
+
+// Weird thing I had to do to get the Logging library working with teensy
+extern "C"{
+  int _getpid(){ return -1;}
+  int _kill(int pid, int sig){ return -1; }
+  int _write(){return -1;}
+}
+
+#define LOGLEVEL LOG_LEVEL_INFOS
+
+using namespace pixelmap;
+
+Input* input;
+
+// Curtains
+LEDStrip strip1 = LEDStrip(150);
+LEDStrip strip2 = LEDStrip(150);
+
+// Curtain LEDs
+LEDs leds1 = LEDs(&strip1, 0, 150);
+LEDs leds2 = LEDs(&strip2, 0, 150);
+
+Visualization* viz;
+CurtainAnimation* anim1;
+CurtainAnimation* anim2;
+
+// Audio shield setup
+AudioInputI2S audio;
+//AudioAnalyzePeak peak;
+//AudioAnalyzeNoteFrequency note;
+//AudioConnection patchCord1(audio, peak);
+//AudioConnection patchCord2(audio, note);
+AudioControlSGTL5000 audioShield;
+AudioAnalyzeFFT1024 fft;
+AudioConnection patchCord1(audio, 0, fft, 0);
+
+void setup() {
+  Log.Init(LOGLEVEL, 9600);
+  delay(1000);
+  Log.Info("Starting setup()\n");
+  Serial.flush();
+  delay(1000);
+
+  AudioMemory(12);
+  audioShield.enable();
+  audioShield.inputSelect(AUDIO_INPUT_LINEIN);
+  audioShield.lineInLevel(15);
+  //audioShield.inputSelect(AUDIO_INPUT_MIC);
+  //audioShield.micGain(100);
+  //note.begin(.99);
+
+  fft.windowFunction(AudioWindowHanning1024);
+
+  //input = new AudioShieldInput(&peak, &note);
+  input = new FFTInput(&fft);
+
+  viz = new RippleVisualization(input, 35, 1, true);
+
+  anim1 = new CurtainAnimation(viz, leds1);
+  anim2 = new CurtainAnimation(viz, leds2);
+
+  // Inner right  curtain
+  anim1->init(
+     25,    // height in pixels
+     6,     // width in pixels
+     1.0,   // height in ratio of visualiation
+     0.17,  // width in ratio of visualiation
+     0.0,   // rotation in radians
+     0.25,   // x distance from visualization start in ratio of visualization
+    -0.5,   // y "
+     0.0);  // z "
+
+  // Inner left curtain
+  anim2->init(
+     25,    // height in pixels
+     6,     // width in pixels
+     1.0,   // height in ratio of visualiation
+     0.17,  // width in ratio of visualiation
+     0.0,   // rotation in radians
+    -0.50,  // x distance from visualization start in ratio of visualization
+    -0.5,   // y "
+     0.0);  // z "
+
+  FastLED.addLeds<WS2811,  7, RGB>(strip1.leds, 150);
+  FastLED.addLeds<WS2811, 14, RGB>(strip2.leds, 150);
+
+  FastLED.setBrightness(255);
+
+  Looper* looper = Looper::instance();
+  looper->addInput(input);
+  looper->addVisualization(viz);
+  looper->addAnimation(anim1);
+  looper->addAnimation(anim2);
+  looper->setUpdatesPerSecond(30);
+
+  Log.Info("Finished setup()\n");
+  delay(100);
+}
+
+void loop() {
+  AudioNoInterrupts();
+  Looper::instance()->loop();
+  AudioInterrupts();
+}
